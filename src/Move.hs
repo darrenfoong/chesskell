412e2dda85a4ee7ef9acb9fb514b3427e19871a5
Darren Foong <darrenfoong@gmail.com>
2019-11-27 23:42:52 +0800

    Refactor validMovePiece

--- src/Move.hs
+++ src/Move.hs
@@ -4,8 +4,8 @@ module Move
   validMovePiece
 ) where
 
-import Types (Piece(..), Color(..), CPiece(..), Board, Position, Move)
-import Position (mkPos, mkPositions, validPos)
+import Types (Piece(..), Color(..), CPiece(..), Move)
+import Position (mkPos, validPos)
 
 parseMove :: String -> Either String Move
 parseMove moveStr@(sc:sr:ec:er:_) = let start = mkPos (sc,sr)
@@ -16,19 +16,25 @@ parseMove moveStr@(sc:sr:ec:er:_) = let start = mkPos (sc,sr)
 parseMove moveStr = Left $ "ERROR: Invalid move string: " ++ moveStr
 
 validMovePiece :: CPiece -> Bool -> Move -> Bool
-validMovePiece Null            _      _                  = False
-validMovePiece (CP _ King)     _     ((sc,sr), (ec,er)) = (ec == sc && abs(er-sr) == 1) ||
-                                                          (er == sr && abs(ec-sc) == 1) ||
-                                                          ((ec-sc) == (er-sr) && abs(ec-sc) == 1)
-validMovePiece (CP color Queen) attack move             = validMovePiece (CP color Rook) attack move ||
-                                                          validMovePiece (CP color Bishop) attack move
-validMovePiece (CP _ Rook)     _     ((sc,sr), (ec,er)) = ec == sc || er == sr
-validMovePiece (CP _ Bishop)   _     ((sc,sr), (ec,er)) = abs(ec-sc) == abs(er-sr)
-validMovePiece (CP _ Knight)   _     ((sc,sr), (ec,er)) = let cdiff = (ec-sc)
-                                                              rdiff = (er-sr) in
-                                                              (abs cdiff == 1 && abs rdiff == 2) ||
-                                                              (abs cdiff == 2 && abs rdiff == 1)
-validMovePiece (CP Black Pawn) True  ((sc,sr), (ec,er)) = abs(ec-sc) == 1 && (er-sr) == (-1)
-validMovePiece (CP White Pawn) True  ((sc,sr), (ec,er)) = abs(ec-sc) == 1 && (er-sr) == 1
-validMovePiece (CP Black Pawn) False ((sc,sr), (ec,er)) = ec == sc && ((er-sr) == (-1) || (sr == 7 && (er-sr) == (-2)))
-validMovePiece (CP White Pawn) False ((sc,sr), (ec,er)) = ec == sc && ((er-sr) == 1 || (sr == 2 && (er-sr) == 2))
+validMovePiece (CP Black Pawn) False ((sc,sr), (ec,er)) = let cdiff = ec-sc
+                                                              rdiff = er-sr
+                                                          in  cdiff == 0 && (rdiff == (-1) || (sr == 7 && rdiff == (-2)))
+validMovePiece (CP White Pawn) False ((sc,sr), (ec,er)) = let cdiff = ec-sc
+                                                              rdiff = er-sr
+                                                          in  cdiff == 0 && (rdiff == 1 || (sr == 2 && rdiff == 2))
+validMovePiece piece attack ((sc,sr), (ec,er)) = validMovePieceInner piece attack (ec-sc, er-sr)
+
+validMovePieceInner :: CPiece -> Bool -> (Int, Int) -> Bool
+validMovePieceInner Null            _      _             = False
+validMovePieceInner (CP _ King)     _     (cdiff, rdiff) = (cdiff == 0 && abs rdiff == 1) ||
+                                                           (rdiff == 0 && abs cdiff == 1) ||
+                                                           (cdiff == rdiff && abs cdiff == 1)
+validMovePieceInner (CP color Queen) attack move         = validMovePieceInner (CP color Rook) attack move ||
+                                                           validMovePieceInner (CP color Bishop) attack move
+validMovePieceInner (CP _ Rook)     _     (cdiff, rdiff) = cdiff == 0 || rdiff == 0
+validMovePieceInner (CP _ Bishop)   _     (cdiff, rdiff) = abs cdiff == abs rdiff
+validMovePieceInner (CP _ Knight)   _     (cdiff, rdiff) = (abs cdiff == 1 && abs rdiff == 2) ||
+                                                               (abs cdiff == 2 && abs rdiff == 1)
+validMovePieceInner (CP Black Pawn) True  (cdiff, rdiff) = abs cdiff == 1 && rdiff == (-1)
+validMovePieceInner (CP White Pawn) True  (cdiff, rdiff) = abs cdiff == 1 && rdiff == 1
+validMovePieceInner _               _     _              = False
