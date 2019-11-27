cf337e8138865ecc4fba15288e68716403d3bc70
Darren Foong <darrenfoong@gmail.com>
2019-11-27 23:48:52 +0800

    Refactor validMovePiece

--- src/Move.hs
+++ src/Move.hs
@@ -25,16 +25,15 @@ validMovePiece (CP White Pawn) False ((sc,sr), (ec,er)) = let cdiff = ec-sc
 validMovePiece piece attack ((sc,sr), (ec,er)) = validMovePieceInner piece attack (ec-sc, er-sr)
 
 validMovePieceInner :: CPiece -> Bool -> (Int, Int) -> Bool
-validMovePieceInner Null            _      _             = False
 validMovePieceInner (CP _ King)     _     (cdiff, rdiff) = (cdiff == 0 && abs rdiff == 1) ||
                                                            (rdiff == 0 && abs cdiff == 1) ||
-                                                           (cdiff == rdiff && abs cdiff == 1)
-validMovePieceInner (CP color Queen) attack move         = validMovePieceInner (CP color Rook) attack move ||
+                                                           (abs cdiff == abs rdiff && abs cdiff == 1)
+validMovePieceInner (CP color Queen) attack move         = validMovePieceInner (CP color Rook)   attack move ||
                                                            validMovePieceInner (CP color Bishop) attack move
 validMovePieceInner (CP _ Rook)     _     (cdiff, rdiff) = cdiff == 0 || rdiff == 0
 validMovePieceInner (CP _ Bishop)   _     (cdiff, rdiff) = abs cdiff == abs rdiff
 validMovePieceInner (CP _ Knight)   _     (cdiff, rdiff) = (abs cdiff == 1 && abs rdiff == 2) ||
-                                                               (abs cdiff == 2 && abs rdiff == 1)
+                                                           (abs cdiff == 2 && abs rdiff == 1)
 validMovePieceInner (CP Black Pawn) True  (cdiff, rdiff) = abs cdiff == 1 && rdiff == (-1)
 validMovePieceInner (CP White Pawn) True  (cdiff, rdiff) = abs cdiff == 1 && rdiff == 1
 validMovePieceInner _               _     _              = False
