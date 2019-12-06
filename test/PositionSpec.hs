2af903d8ea3fce6b66c06f7390cb9a0da6048a9a
Darren Foong <darrenfoong@gmail.com>
2019-12-06 12:55:50 +0200

    Update PositionSpec

--- test/PositionSpec.hs
+++ test/PositionSpec.hs
@@ -2,7 +2,7 @@ module PositionSpec where
 
 import Test.Hspec
 
-import Position(mkPos)
+import Position(mkPos, validPos)
 
 {- HLINT ignore "Redundant do" -}
 spec :: Spec
@@ -12,3 +12,11 @@ spec = do
 
     it "(e, 2) is supposed to be (5, 2)" $ do
       mkPos ('e', '2') `shouldBe` (5, 2)
+
+  describe "Validate validPos" $ do
+
+    it "(1, 1) is supposed to be valid" $ do
+      validPos (1, 1) `shouldBe` True
+
+    it "(9, 14) is supposed to be invalid" $ do
+      validPos (9, 14) `shouldBe` False
