0d5e5712648a0b9bbd60c168b6f854358618eac4
Darren Foong <darrenfoong@gmail.com>
2021-05-07 17:39:18 +0800

    Add MainWeb.hs

---
+++ README.md
@@ -0,0 +1,14 @@
+# chesskell
+
+## Building
+
+```
+stack build
+```
+
+## Running
+
+```
+stack run chesskell     # command line UI
+stack run chesskell-web # web UI
+```
---
+++ src/MainWeb.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ExtendedDefaultRules  #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE ViewPatterns          #-}
+
+import Control.Applicative ((<$>), (<*>))
+import Data.Text (Text, pack)
+import Yesod
+import Yesod.Form
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+/oof NotFoundR GET
+|]
+
+instance Yesod App where
+    defaultLayout = appLayout
+    errorHandler NotFound = redirect NotFoundR
+    errorHandler other = defaultErrorHandler other
+
+appLayout :: Widget -> Handler Html
+appLayout widget = do
+    pc <- widgetToPageContent $ do
+          widget
+          toWidgetHead [hamlet|<meta name="keywords" content="anki">|]
+          toWidget [lucius|
+          body { font: 1.0rem/1.1 sans-serif; }
+          #content { padding: 10px; }
+          |]
+    withUrlRenderer
+          [hamlet|
+          $doctype 5
+          <html>
+            <head>
+              <title>#{pageTitle pc}
+              ^{pageHead pc}
+            <body>
+              <div #content>
+                ^{pageBody pc}
+          |]
+
+getHomeR = defaultLayout $ do
+        setTitle "addki"
+        toWidget [hamlet|<h1>chesskell|]
+        toWidget [hamlet|
+        <p>Test
+        |]
+
+
+getNotFoundR = defaultLayout $ do
+    setTitle "Not found"
+    toWidget [hamlet|<h1>Oof!|]
+    toWidget [hamlet|
+    <p>We couldn't find your page
+    |]
+
+main :: IO ()
+main = warp 3000 App
