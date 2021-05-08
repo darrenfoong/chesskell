34f1ebd09dec05f57957a164b87a8a26bd873914
Darren Foong <darrenfoong@gmail.com>
2021-05-08 12:46:40 +0800

    Update grid

--- src/MainWeb.hs
+++ src/MainWeb.hs
@@ -6,13 +6,34 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ViewPatterns #-}
 
+import Board (getPiece, mkBoard, parseMove)
 import Control.Applicative ((<$>), (<*>))
 import Data.Char
+import Data.Maybe
 import Data.Text (Text, pack)
+import System.Random
+import Types (CPiece (..), Color (..), Piece (..))
 import Yesod
 import Yesod.Form
 
 data App = App
+
+data State = Start | PendingMoveStart | PendingMoveEnd deriving (Eq)
+
+prettyPrintPiece :: CPiece -> String
+prettyPrintPiece (CP Black King) = "♚"
+prettyPrintPiece (CP Black Queen) = "♛"
+prettyPrintPiece (CP Black Rook) = "♜"
+prettyPrintPiece (CP Black Bishop) = "♝"
+prettyPrintPiece (CP Black Knight) = "♞"
+prettyPrintPiece (CP Black Pawn) = "♟︎"
+prettyPrintPiece (CP White King) = "♔"
+prettyPrintPiece (CP White Queen) = "♕"
+prettyPrintPiece (CP White Rook) = "♖"
+prettyPrintPiece (CP White Bishop) = "♗"
+prettyPrintPiece (CP White Knight) = "♘"
+prettyPrintPiece (CP White Pawn) = "♙"
+prettyPrintPiece Types.Null = ""
 
 mkYesod
   "App"
@@ -49,22 +70,61 @@ appLayout widget = do
     |]
 
 getHomeR = do
-  let rs = map (\i -> chr (i + ord 'a' - 1)) [1 .. 8]
-  let cs = reverse [1 .. 8]
   mPosition <- lookupPostParam "position"
+  mStart <- lookupPostParam "start"
+
+  let state =
+        if isNothing mStart && isNothing mPosition
+          then Start
+          else
+            if isJust mStart && isJust mPosition
+              then PendingMoveStart
+              else PendingMoveEnd
+
+  let mPreviousWhiteMove = case state of
+        Start -> Nothing
+        PendingMoveStart -> Nothing
+        PendingMoveEnd -> do
+          start <- mStart
+          end <- mPosition
+          return (show start ++ show end)
+
+  let gen = getStdGen
+  let board = mkBoard
+
+  --  let move = case mPreviousWhiteMove of
+  --                Just previousWhiteMove -> parseMove previousWhiteMove
+  --                Nothing -> Left "nah"
+
+  let mPreviousBlackMove = Just "e5e5"
+
   defaultLayout $ do
     setTitle "chesskell"
     [whamlet|
-      $maybe position <- mPosition
-        <p>#{position}
-      $nothing
-        <p>Welcome
+      <p>You are White.
+      $if state /= Start
+        $maybe previousBlackMove <- mPreviousBlackMove
+          <p>Black played: #{previousBlackMove}
+
+      $if state /= PendingMoveEnd
+        <p>Please select a piece.
+      $else
+        $maybe position <- mPosition
+          <p>You selected <span>#{position}</span>. Please select where the piece should go.
+    |]
+    let rs = [1 .. 8]
+    let cs = reverse [1 .. 8]
+    let rconv = \i -> chr (i + ord 'a' - 1)
+    [whamlet|
       <form method="post" action=@{HomeR}>
+        $if state == PendingMoveEnd
+          $maybe position <- mPosition
+            <input type="hidden" name="start" value="#{position}">
         <table>
           $forall r <- rs
             <tr>
               $forall c <- cs
-                <td><input type="submit" name="position" value="#{r}#{c}">
+                <td><button name="position" value="#{rconv r}#{c}">#{prettyPrintPiece $ getPiece board (c, r)}
     |]
 
 postHomeR = getHomeR
