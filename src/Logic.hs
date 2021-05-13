8f10fe7bcd63bc5afd40d582faf321a1a35e97aa
Darren Foong <darrenfoong@gmail.com>
2021-05-13 13:24:41 +0800

    Tidy

--- src/Logic.hs
+++ src/Logic.hs
@@ -39,11 +39,7 @@ scorePiece (CP _ Pawn) = 1
 scorePiece Null = 0
 
 isInCheckmate :: Color -> Board -> Bool
-isInCheckmate color board =
-  let possibleMoves = genMoves board color
-      possibleNextBoards = map (\m -> fromRight [] $ advanceBoard board m color) possibleMoves
-      filteredPossibleNextBoards = filter (/= []) possibleNextBoards
-   in all (isInCheck color) (board : filteredPossibleNextBoards)
+isInCheckmate color board = all (isInCheck color) ((:) board $ map snd $ genNextMoveBoards board color)
 
 isInCheck :: Color -> Board -> Bool
 isInCheck color board = maybe False (isUnderAttack color board) (getKingPosition color board)
@@ -71,18 +67,24 @@ genMove gen board color =
    in (gen2, Just m)
 
 minimax :: ([Move] -> [Move]) -> Board -> Color -> Color -> Int -> Bool -> (Int, Move)
-minimax f board scoringColor playerColor 1 maximising = maximumBy (compareMove maximising) $ map (\m -> (scoreBoard scoringColor (movePiece board m), m)) $ f $ genNonCheckMoves board playerColor
-minimax f board scoringColor playerColor n maximising = maximumBy (compareMove maximising) $ map (\m -> let (s, _) = minimax f (movePiece board m) scoringColor (swapColor playerColor) (n -1) (not maximising) in (s, m)) $ f $ genNonCheckMoves board playerColor
+minimax f board scoringColor playerColor n maximising =
+  let g =
+        if n == 1
+          then \m -> (scoreBoard scoringColor $ movePiece board m, m)
+          else \m -> let (s, _) = minimax f (movePiece board m) scoringColor (swapColor playerColor) (n -1) (not maximising) in (s, m)
+   in maximumBy (compareMove maximising) $ map g $ f $ genNonCheckMoves board playerColor
 
 compareMove :: Bool -> (Int, Move) -> (Int, Move) -> Ordering
 compareMove maximising (s1, _) (s2, _) = if maximising then compare s1 s2 else compare s2 s1
 
-genNonCheckMoves :: Board -> Color -> [Move]
-genNonCheckMoves board color =
+genNextMoveBoards :: Board -> Color -> [(Move, Board)]
+genNextMoveBoards board color =
   let possibleMoves = genMoves board color
-      possibleNextMoveBoards = map (\m -> (m, fromRight [] $ advanceBoard board m color)) possibleMoves
-      filteredPossibleNextMoveBoards = filter (\(_, b) -> b /= []) possibleNextMoveBoards
-   in map fst $ filter (\(_, b) -> not $ isInCheck color b) filteredPossibleNextMoveBoards
+      possibleMoveBoards = map (\m -> (m, fromRight [] $ advanceBoard board m color)) possibleMoves
+   in filter (\(_, b) -> b /= []) possibleMoveBoards
+
+genNonCheckMoves :: Board -> Color -> [Move]
+genNonCheckMoves board color = map fst $ filter (\(_, b) -> not $ isInCheck color b) $ genNextMoveBoards board color
 
 genMoves :: Board -> Color -> [Move]
 genMoves board color = concatMap (genPossibleMovesPiece board) (genPositions board color)
