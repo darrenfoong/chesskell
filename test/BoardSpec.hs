4f0537a960df14bb2f5de31fc62cb7e634bd1037
Darren Foong <darrenfoong@gmail.com>
2021-06-22 22:48:32 +0800

    Update BoardSpec.hs

--- test/BoardSpec.hs
+++ test/BoardSpec.hs
@@ -1,15 +1,51 @@
 module BoardSpec where
 
-import Board (mkBoard, readBoard, writeBoard)
+import Board (advanceBoard, readBoard, writeBoard)
+import Data.Either (fromRight)
 import Data.Text (pack)
 import Test.Hspec
+import Types (Color (..))
 
 {- HLINT ignore "Redundant do" -}
 spec :: Spec
 spec = do
   describe "writeBoard and readBoard" $ do
     it "serialize properly" $ do
-      let deserializedBoard = case readBoard $ pack $ writeBoard mkBoard of
+      let boardStr =
+            "rnbqkbnr\
+            \pppppppp\
+            \########\
+            \########\
+            \########\
+            \########\
+            \PPPPPPPP\
+            \RNBQKBNR"
+      let deserializedBoard = case readBoard $ pack boardStr of
             Left _ -> []
             Right board -> board
-      writeBoard deserializedBoard `shouldBe` writeBoard mkBoard
+      writeBoard deserializedBoard `shouldBe` boardStr
+
+  describe "advanceBoard" $ do
+    it "accepts valid castling (black short)" $ do
+      let board =
+            fromRight [] $
+              readBoard $
+                pack
+                  "rnbqkbnr\
+                  \pppppppp\
+                  \########\
+                  \########\
+                  \########\
+                  \########\
+                  \PPPPPPPP\
+                  \RNBQK##R"
+      let advancedBoard = fromRight [] $ advanceBoard board Black ((5, 8), (7, 8))
+      writeBoard advancedBoard
+        `shouldBe` "rnbqkbnr\
+                   \pppppppp\
+                   \########\
+                   \########\
+                   \########\
+                   \########\
+                   \PPPPPPPP\
+                   \RNBQ#SL#"
